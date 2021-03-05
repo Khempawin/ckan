0baf1cbf43ec6e2dc16e5ac25b7a35f3d67d1f26
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2021-03-05 10:00:14 +0700

    Add Thai tag compatibility

--- ckan/logic/validators.py
+++ ckan/logic/validators.py
@@ -452,7 +452,7 @@ def tag_length_validator(value, context):
 
 def tag_name_validator(value, context):
 
-    tagname_match = re.compile('[\w \-.]*$', re.UNICODE)
+    tagname_match = re.compile('[\w \-.\u0E01-\u0E4E]*$', re.UNICODE)
     if not tagname_match.match(value):
         raise Invalid(_('Tag "%s" must be alphanumeric '
                         'characters or symbols: -_.') % (value))
